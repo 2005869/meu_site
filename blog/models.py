c2aa6f22d23a92372ff5365e261a58c438b1ec61
2005869 <2005869@aluno.univesp..br>
2020-06-19 16:06:21 -0300

    0230

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Model):
+    def get_queryset(self):
+        return super(PublishedManager).self.get_queryset().filter(status='publicado')
+
+class Post(models.Model):
+    STATUS = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado'),
+    )
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    autor = models.ForeignKey(User,
+                              on_delete=models.CASCADE)
+    conteudo = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now())
+    criado = models.DateTimeField(auto_now_add=True)
+    alterado = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS,
+                              default='rascunho')
+    objects = models.Manager()
+    published = PublishedManager()
+    class Meta:
+        ordering = ('-publicado',)
+    def __str__(self):
+        return '{} - {}' .format(self.titulo, self.slug)
